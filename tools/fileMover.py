c22e402cdf7fbba2fb59faf747ba7ed27ff6bde0
Taejeong <monttj@gmail.com>
2014-10-31 20:37:04 +0900

    update configuration file for copying files to local

--- tools/fileMover.py
+++ tools/fileMover.py
@@ -8,21 +8,35 @@ import sys
 import time
 import commands
 
+#voms-proxy-init -voms cms
+dirBrussels="/pnfs/iihe/cms/store/user/dhondt/FCN_13TeV_IIHE_IPHC/Gridpacks/"
+
 dir="/home/cms/Prod_13TeV/Gridpacks/"
 
-sample="TTsemilep_madspin_2"
+samples=["TTsemilep_madspin_1"]
+#samples = ["ZToLL50-0Jet_sm-no_masses"]
+#samples = ["HToAA"]
+#samples = ["TTdilep_madspin"]
+#samples = ["WToLNu-2Jets_sm-no_masses"]
+#samples = ["ZToLL50-2Jets_sm-no_masses"]
+#samples = ["AA"]
+#samples = ["WHToAA"]
+#samples += ["ZHToAA"]
 
-cmd="lcg-ls --verbose -D  srmv2 --nobdii srm://sbgse1.in2p3.fr:8446/srm/managerv2\?SFN=/dpm/in2p3.fr"+dir+sample
+for sample in samples:
 
-fileNames = commands.getoutput(cmd).split("\n")
+  cmd="lcg-ls --verbose -D  srmv2 --nobdii srm://sbgse1.in2p3.fr:8446/srm/managerv2\?SFN=/dpm/in2p3.fr"+dir+sample
+#cmd = "lcg-ls --verbose -D srmv2 --nobdii srm://maite.iihe.ac.be:8443/srm/managerv2\?SFN="
+  fileNames = commands.getoutput(cmd).split("\n")
 
-dest="/cms/home/tjkim/fcnc/sample/"+sample
+  dest="/cms/home/tjkim/fcnc/sample/"+sample
 
-os.system("mkdir "+dest)
+  os.system("mkdir "+dest)
 
-for fileName in fileNames:
-  if fileName.endswith("root") > 0:
-    print "copying..." + fileName
-    files = fileName.split("/")
-    destfile = files[len(files)-1]
-    os.system("lcg-cp -b -D srmv2 --vo cms  srm://sbgse1.in2p3.fr:8446/srm/managerv2\?SFN="+fileName+" "+dest+"/"+destfile)
+  for fileName in fileNames:
+    print fileName
+    if fileName.endswith("root") > 0:
+      print "copying..." + fileName
+      files = fileName.split("/")
+      destfile = files[len(files)-1]
+      os.system("lcg-cp -b -D srmv2 --vo cms  srm://sbgse1.in2p3.fr:8446/srm/managerv2\?SFN="+fileName+" "+dest+"/"+destfile)
