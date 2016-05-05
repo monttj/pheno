582330616d8fe9d891ff8dbbe9cc19705c601d70
TAE JEONG KIM <monttj@gmail.com>
2016-05-05 10:23:41 +0900

    example for multiple files

---
+++ topmass/Cards/job.sh
@@ -0,0 +1,121 @@
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+./bin/generate_events -f
+
+gunzip  Events/run_41/tag_1_pythia_events.hep.gz
+gunzip  Events/run_42/tag_1_pythia_events.hep.gz
+gunzip  Events/run_43/tag_1_pythia_events.hep.gz
+gunzip  Events/run_44/tag_1_pythia_events.hep.gz
+gunzip  Events/run_45/tag_1_pythia_events.hep.gz
+gunzip  Events/run_46/tag_1_pythia_events.hep.gz
+gunzip  Events/run_47/tag_1_pythia_events.hep.gz
+gunzip  Events/run_48/tag_1_pythia_events.hep.gz
+gunzip  Events/run_49/tag_1_pythia_events.hep.gz
+gunzip  Events/run_50/tag_1_pythia_events.hep.gz
+gunzip  Events/run_51/tag_1_pythia_events.hep.gz
+gunzip  Events/run_52/tag_1_pythia_events.hep.gz
+gunzip  Events/run_53/tag_1_pythia_events.hep.gz
+gunzip  Events/run_54/tag_1_pythia_events.hep.gz
+gunzip  Events/run_55/tag_1_pythia_events.hep.gz
+gunzip  Events/run_56/tag_1_pythia_events.hep.gz
+gunzip  Events/run_57/tag_1_pythia_events.hep.gz
+gunzip  Events/run_58/tag_1_pythia_events.hep.gz
+gunzip  Events/run_59/tag_1_pythia_events.hep.gz
+gunzip  Events/run_60/tag_1_pythia_events.hep.gz
+gunzip  Events/run_61/tag_1_pythia_events.hep.gz
+gunzip  Events/run_62/tag_1_pythia_events.hep.gz
+gunzip  Events/run_63/tag_1_pythia_events.hep.gz
+gunzip  Events/run_64/tag_1_pythia_events.hep.gz
+gunzip  Events/run_65/tag_1_pythia_events.hep.gz
+gunzip  Events/run_66/tag_1_pythia_events.hep.gz
+gunzip  Events/run_67/tag_1_pythia_events.hep.gz
+gunzip  Events/run_68/tag_1_pythia_events.hep.gz
+gunzip  Events/run_69/tag_1_pythia_events.hep.gz
+gunzip  Events/run_70/tag_1_pythia_events.hep.gz
+gunzip  Events/run_71/tag_1_pythia_events.hep.gz
+gunzip  Events/run_72/tag_1_pythia_events.hep.gz
+gunzip  Events/run_73/tag_1_pythia_events.hep.gz
+gunzip  Events/run_74/tag_1_pythia_events.hep.gz
+gunzip  Events/run_75/tag_1_pythia_events.hep.gz
+gunzip  Events/run_76/tag_1_pythia_events.hep.gz
+gunzip  Events/run_77/tag_1_pythia_events.hep.gz
+gunzip  Events/run_78/tag_1_pythia_events.hep.gz
+gunzip  Events/run_79/tag_1_pythia_events.hep.gz
+gunzip  Events/run_80/tag_1_pythia_events.hep.gz
+
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_41.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_41/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_42.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_42/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_43.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_43/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_44.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_44/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_45.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_45/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_46.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_46/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_47.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_47/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_48.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_48/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_49.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_49/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_50.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_50/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_51.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_51/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_52.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_52/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_53.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_53/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_54.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_54/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_55.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_55/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_56.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_56/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_57.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_57/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_58.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_58/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_59.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_59/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_60.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_60/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_61.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_61/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_62.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_62/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_63.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_63/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_64.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_64/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_65.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_65/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_66.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_66/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_67.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_67/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_68.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_68/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_69.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_69/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_70.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_70/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_71.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_71/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_72.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_72/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_73.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_73/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_74.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_74/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_75.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_75/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_76.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_76/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_77.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_77/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_78.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_78/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_79.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_79/tag_1_pythia_events.hep
+DelphesSTDHEP ~/work/delphes/cards/delphes_card_CMS_topmass.tcl /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/ttbar_LO_80.root /cms/scratch/tjkim/aMCNLO/MG5_aMC_v2_3_3/ttbar_LO_LPJ_2/Events/run_80/tag_1_pythia_events.hep
