e63860ddb53c8b47494a0e361d6eb5f4744d5adf
TAE JEONG KIM <monttj@gmail.com>
2017-02-21 14:49:30 +0900

    update unfold code

--- topmass/unfold.C
+++ topmass/unfold.C
@@ -2,59 +2,64 @@ void unfold(){
 
   gSystem->Load("/Users/tjkim/Work/Software/RooUnfold/libRooUnfold");
 
-  TFile * f_167 = new TFile("sayaka/sayaka_3jets1b/hist_LO_167_sayaka_600K.root","read");
-  TFile * f_170 = new TFile("sayaka/sayaka_3jets1b/hist_LO_170_sayaka_600K.root","read");
-  TFile * f_173 = new TFile("sayaka/sayaka_3jets1b/hist_LO_173_sayaka_600K.root","read");
-  TFile * f_176 = new TFile("sayaka/sayaka_3jets1b/hist_LO_176_sayaka_600K.root","read");
-  TFile * f_179 = new TFile("sayaka/sayaka_3jets1b/hist_LO_179_sayaka_600K.root","read");
-  TFile * f_scaleup = new TFile("sayaka/sayaka_3jets1b/q2scale/hist_LO_173_sayaka_scale20.root","read");
-  TFile * f_scalect = new TFile("sayaka/sayaka_3jets1b/q2scale/hist_LO_173_sayaka_scale10.root","read");
-  TFile * f_scaledw = new TFile("sayaka/sayaka_3jets1b/q2scale/hist_LO_173_sayaka_scale05.root","read");
+  bool scalestudy = false;
 
+  TFile * f_167 = new TFile("sayaka/pt/hist_LO_167_sayaka_600K_3.root","read");
+  TFile * f_170 = new TFile("sayaka/pt/hist_LO_170_sayaka_600K_3.root","read");
+  TFile * f_173 = new TFile("sayaka/pt/hist_LO_173_sayaka_600K_3.root","read");
+  TFile * f_176 = new TFile("sayaka/pt/hist_LO_176_sayaka_600K_3.root","read");
+  TFile * f_179 = new TFile("sayaka/pt/hist_LO_179_sayaka_600K_3.root","read");
 
   TH1F * h_mea_S1_167 = (TH1F *) f_167->Get("h_lepton_energy_reco");
   TH1F * h_mea_S1_170 = (TH1F *) f_170->Get("h_lepton_energy_reco");
   TH1F * h_mea_S1_173 = (TH1F *) f_173->Get("h_lepton_energy_reco");
   TH1F * h_mea_S1_176 = (TH1F *) f_176->Get("h_lepton_energy_reco");
   TH1F * h_mea_S1_179 = (TH1F *) f_179->Get("h_lepton_energy_reco");
-  TH1F * h_mea_S1_scaleup = (TH1F *) f_scaleup->Get("h_lepton_energy_reco");
-  TH1F * h_mea_S1_scalect = (TH1F *) f_scalect->Get("h_lepton_energy_reco");
-  TH1F * h_mea_S1_scaledw = (TH1F *) f_scaledw->Get("h_lepton_energy_reco");
- 
 
   TH1F * h_mea_167 = (TH1F *) f_167->Get("h_lepton_energy_reco_final");
   TH1F * h_mea_170 = (TH1F *) f_170->Get("h_lepton_energy_reco_final");
   TH1F * h_mea_173 = (TH1F *) f_173->Get("h_lepton_energy_reco_final");
   TH1F * h_mea_176 = (TH1F *) f_176->Get("h_lepton_energy_reco_final");
   TH1F * h_mea_179 = (TH1F *) f_179->Get("h_lepton_energy_reco_final");
-  TH1F * h_mea_scaleup = (TH1F *) f_scaleup->Get("h_lepton_energy_reco_final");
-  TH1F * h_mea_scalect = (TH1F *) f_scalect->Get("h_lepton_energy_reco_final");
-  TH1F * h_mea_scaledw = (TH1F *) f_scaledw->Get("h_lepton_energy_reco_final");
 
   h_mea_S1_167->Rebin(20);
   h_mea_S1_170->Rebin(20);
   h_mea_S1_173->Rebin(20);
   h_mea_S1_176->Rebin(20);
   h_mea_S1_179->Rebin(20); 
-  h_mea_S1_scaleup->Rebin(20); 
-  h_mea_S1_scalect->Rebin(20); 
-  h_mea_S1_scaledw->Rebin(20); 
 
   h_mea_167->Rebin(20);
   h_mea_170->Rebin(20);
   h_mea_173->Rebin(20);
   h_mea_176->Rebin(20);
   h_mea_179->Rebin(20);
-  h_mea_scaleup->Rebin(20);
-  h_mea_scalect->Rebin(20);
-  h_mea_scaledw->Rebin(20);
+
+  if( scalestudy ) {
+
+    TFile * f_scaleup = new TFile("sayaka/pt/q2scale/hist_LO_173_sayaka_scale20.root","read");
+    TFile * f_scalect = new TFile("sayaka/pt/q2scale/hist_LO_173_sayaka_scale10.root","read");
+    TFile * f_scaledw = new TFile("sayaka/pt/q2scale/hist_LO_173_sayaka_scale05.root","read");
+
+    TH1F * h_mea_S1_scaleup = (TH1F *) f_scaleup->Get("h_lepton_energy_reco");
+    TH1F * h_mea_S1_scalect = (TH1F *) f_scalect->Get("h_lepton_energy_reco");
+    TH1F * h_mea_S1_scaledw = (TH1F *) f_scaledw->Get("h_lepton_energy_reco");
+
+    TH1F * h_mea_scaleup = (TH1F *) f_scaleup->Get("h_lepton_energy_reco_final");
+    TH1F * h_mea_scalect = (TH1F *) f_scalect->Get("h_lepton_energy_reco_final");
+    TH1F * h_mea_scaledw = (TH1F *) f_scaledw->Get("h_lepton_energy_reco_final");
+
+    h_mea_scaleup->Rebin(20);
+    h_mea_scalect->Rebin(20);
+    h_mea_scaledw->Rebin(20);
+
+  }
   
 
-  TFile * f_response_167 = new TFile("sayaka/sayaka_3jets1b/hist_LO_167_sayaka_1200K.root","read");
-  TFile * f_response_170 = new TFile("sayaka/sayaka_3jets1b/hist_LO_170_sayaka_1200K.root","read");
-  TFile * f_response_173 = new TFile("sayaka/sayaka_3jets1b/hist_LO_173_sayaka_1200K_merged.root","read");
-  TFile * f_response_176 = new TFile("sayaka/sayaka_3jets1b/hist_LO_176_sayaka_1200K.root","read");
-  TFile * f_response_179 = new TFile("sayaka/sayaka_3jets1b/hist_LO_179_sayaka_1200K.root","read");
+  TFile * f_response_167 = new TFile("sayaka/pt/hist_LO_167_sayaka_1200K.root","read");
+  TFile * f_response_170 = new TFile("sayaka/pt/hist_LO_170_sayaka_1200K.root","read");
+  TFile * f_response_173 = new TFile("sayaka/pt/hist_LO_173_sayaka_1200K.root","read");
+  TFile * f_response_176 = new TFile("sayaka/pt/hist_LO_176_sayaka_1200K.root","read");
+  TFile * f_response_179 = new TFile("sayaka/pt/hist_LO_179_sayaka_1200K.root","read");
 
   TH1F * h_res_mea_S1_167 = (TH1F *) f_response_167->Get("h_lepton_energy_reco");
   TH1F * h_res_mea_S1_170 = (TH1F *) f_response_170->Get("h_lepton_energy_reco");
@@ -102,32 +107,42 @@ void unfold(){
       double cor173 = h_mea_S1_173->GetBinContent(i)/h_mea_173->GetBinContent(i);
       double cor176 = h_mea_S1_176->GetBinContent(i)/h_mea_176->GetBinContent(i);
       double cor179 = h_mea_S1_179->GetBinContent(i)/h_mea_179->GetBinContent(i);
-      double cor_scaleup = h_mea_S1_scaleup->GetBinContent(i)/h_mea_scaleup->GetBinContent(i);
-      double cor_scalect = h_mea_S1_scalect->GetBinContent(i)/h_mea_scalect->GetBinContent(i);
-      double cor_scaledw = h_mea_S1_scaledw->GetBinContent(i)/h_mea_scaledw->GetBinContent(i);
 
       double num167 = h_mea_167->GetBinContent(i) * cor167;
       double num170 = h_mea_170->GetBinContent(i) * cor170;
       double num173 = h_mea_173->GetBinContent(i) * cor173;
       double num176 = h_mea_176->GetBinContent(i) * cor176;
       double num179 = h_mea_179->GetBinContent(i) * cor179;
-      double num_scaleup = h_mea_scaleup->GetBinContent(i) * cor_scaleup;
-      double num_scalect = h_mea_scalect->GetBinContent(i) * cor_scalect;
-      double num_scaledw = h_mea_scaledw->GetBinContent(i) * cor_scaledw;
+
       h_mea_167->SetBinContent(i, num167);
       h_mea_170->SetBinContent(i, num170);
       h_mea_173->SetBinContent(i, num173);
       h_mea_176->SetBinContent(i, num176);
       h_mea_179->SetBinContent(i, num179);
-      h_mea_scaleup->SetBinContent(i, num_scaleup);
-      h_mea_scalect->SetBinContent(i, num_scalect);
-      h_mea_scaledw->SetBinContent(i, num_scaledw);
+  
+      if( scalestudy ){
+
+        double cor_scaleup = h_mea_S1_scaleup->GetBinContent(i)/h_mea_scaleup->GetBinContent(i);
+        double cor_scalect = h_mea_S1_scalect->GetBinContent(i)/h_mea_scalect->GetBinContent(i);
+        double cor_scaledw = h_mea_S1_scaledw->GetBinContent(i)/h_mea_scaledw->GetBinContent(i);
+
+        double num_scaleup = h_mea_scaleup->GetBinContent(i) * cor_scaleup;
+        double num_scalect = h_mea_scalect->GetBinContent(i) * cor_scalect;
+        double num_scaledw = h_mea_scaledw->GetBinContent(i) * cor_scaledw;
+
+        h_mea_scaleup->SetBinContent(i, num_scaleup);
+        h_mea_scalect->SetBinContent(i, num_scalect);
+        h_mea_scaledw->SetBinContent(i, num_scaledw);
+
+      }
     }
   }
 
-  h_mea_scaleup->Scale(2.0);
-  h_mea_scalect->Scale(2.0);
-  h_mea_scaledw->Scale(2.0);
+  if( scalestudy ){
+    h_mea_scaleup->Scale(2.0);
+    h_mea_scalect->Scale(2.0);
+    h_mea_scaledw->Scale(2.0);
+  }
 
   TH2F * m = (TH2F *) f_response_173->Get("h2_lepton_energy_response");
   m->Rebin2D(20,20);
@@ -141,9 +156,11 @@ void unfold(){
   RooUnfold* unfold_173 = new RooUnfoldBinByBin(response, h_mea_173);
   RooUnfold* unfold_176 = new RooUnfoldBinByBin(response, h_mea_176);
   RooUnfold* unfold_179 = new RooUnfoldBinByBin(response, h_mea_179);
-  RooUnfold* unfold_scaleup = new RooUnfoldBinByBin(response, h_mea_scaleup);
-  RooUnfold* unfold_scalect = new RooUnfoldBinByBin(response, h_mea_scalect);
-  RooUnfold* unfold_scaledw = new RooUnfoldBinByBin(response, h_mea_scaledw);
+  if( scalestudy ){
+    RooUnfold* unfold_scaleup = new RooUnfoldBinByBin(response, h_mea_scaleup);
+    RooUnfold* unfold_scalect = new RooUnfoldBinByBin(response, h_mea_scalect);
+    RooUnfold* unfold_scaledw = new RooUnfoldBinByBin(response, h_mea_scaledw);
+  }
   //RooUnfold* unfold_167 = new RooUnfoldInvert(response, h_mea_167);
   //RooUnfold* unfold_170 = new RooUnfoldInvert(response, h_mea_170);
   //RooUnfold* unfold_173 = new RooUnfoldInvert(response, h_mea_173);
@@ -156,7 +173,6 @@ void unfold(){
   //RooUnfold* unfold_179 = new RooUnfoldSvd(response, h_mea_179, 10);
 
 
-
   //RooUnfold::ErrorTreatment err = RooUnfold::kCovariance;
   RooUnfold::ErrorTreatment err = RooUnfold::kCovToy;
   TH1F* h_unfold_167 = (TH1F*) unfold_167->Hreco(err);
@@ -164,9 +180,12 @@ void unfold(){
   TH1F* h_unfold_173 = (TH1F*) unfold_173->Hreco(err);
   TH1F* h_unfold_176 = (TH1F*) unfold_176->Hreco(err);
   TH1F* h_unfold_179 = (TH1F*) unfold_179->Hreco(err);
-  TH1F* h_unfold_scaleup = (TH1F*) unfold_scaleup->Hreco(err);
-  TH1F* h_unfold_scalect = (TH1F*) unfold_scalect->Hreco(err);
-  TH1F* h_unfold_scaledw = (TH1F*) unfold_scaledw->Hreco(err);
+
+  if( scalestudy ){
+    TH1F* h_unfold_scaleup = (TH1F*) unfold_scaleup->Hreco(err);
+    TH1F* h_unfold_scalect = (TH1F*) unfold_scalect->Hreco(err);
+    TH1F* h_unfold_scaledw = (TH1F*) unfold_scaledw->Hreco(err);
+  }
 
   TH1F * h_gen_full = (TH1F *) f_response_173->Get("h_lepton_energy");
   h_gen_full->Rebin(20);
@@ -179,9 +198,11 @@ void unfold(){
       h_unfold_173->SetBinContent(i,h_gen_full_scaled->GetBinContent(i));
       h_unfold_176->SetBinContent(i,h_gen_full_scaled->GetBinContent(i));
       h_unfold_179->SetBinContent(i,h_gen_full_scaled->GetBinContent(i));
-      h_unfold_scaleup->SetBinContent(i,h_gen_full_scaled->GetBinContent(i));
-      h_unfold_scalect->SetBinContent(i,h_gen_full_scaled->GetBinContent(i));
-      h_unfold_scaledw->SetBinContent(i,h_gen_full_scaled->GetBinContent(i));
+      if( scalestudy){
+        h_unfold_scaleup->SetBinContent(i,h_gen_full_scaled->GetBinContent(i));
+        h_unfold_scalect->SetBinContent(i,h_gen_full_scaled->GetBinContent(i));
+        h_unfold_scaledw->SetBinContent(i,h_gen_full_scaled->GetBinContent(i));
+      }
     }else{
       double corr = h_gen_full->GetBinContent(i)/h_gen->GetBinContent(i);
       h_unfold_167->SetBinContent(i,h_unfold_167->GetBinContent(i)*corr);
@@ -189,9 +210,11 @@ void unfold(){
       h_unfold_173->SetBinContent(i,h_unfold_173->GetBinContent(i)*corr);
       h_unfold_176->SetBinContent(i,h_unfold_176->GetBinContent(i)*corr);
       h_unfold_179->SetBinContent(i,h_unfold_179->GetBinContent(i)*corr);
-      h_unfold_scaleup->SetBinContent(i,h_unfold_scaleup->GetBinContent(i)*corr);
-      h_unfold_scalect->SetBinContent(i,h_unfold_scalect->GetBinContent(i)*corr);
-      h_unfold_scaledw->SetBinContent(i,h_unfold_scaledw->GetBinContent(i)*corr);
+      if( scalestudy){
+        h_unfold_scaleup->SetBinContent(i,h_unfold_scaleup->GetBinContent(i)*corr);
+        h_unfold_scalect->SetBinContent(i,h_unfold_scalect->GetBinContent(i)*corr);
+        h_unfold_scaledw->SetBinContent(i,h_unfold_scaledw->GetBinContent(i)*corr);
+      }
     }
   } 
 
@@ -258,9 +281,9 @@ void unfold(){
   h_unfold_173->Draw("Hsame");
   //h_unfold_176->Draw("Hsame");
   //h_unfold_179->Draw("Hsame");
-  h_unfold_scalect->Draw("Hsame");
-  h_unfold_scalect->SetLineColor(2);
-  h_unfold_scalect->SetLineWidth(2); 
+  //h_unfold_scalect->Draw("Hsame");
+  //h_unfold_scalect->SetLineColor(2);
+  //h_unfold_scalect->SetLineWidth(2); 
 
   h_unfold_167->Write("h_unfold_167");
   h_unfold_170->Write("h_unfold_170");
@@ -280,9 +303,11 @@ void unfold(){
   h_unfold_176_dw->Write("h_unfold_176_dw");
   h_unfold_179_dw->Write("h_unfold_179_dw");
 
-  h_unfold_scaleup->Write("h_unfold_scaleup");
-  h_unfold_scalect->Write("h_unfold_scalect");
-  h_unfold_scaledw->Write("h_unfold_scaledw");
+  if( scalestudy ){
+    h_unfold_scaleup->Write("h_unfold_scaleup");
+    h_unfold_scalect->Write("h_unfold_scalect");
+    h_unfold_scaledw->Write("h_unfold_scaledw");
+  }
 
   f->Write();
   f->Close();
