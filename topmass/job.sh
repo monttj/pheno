f2d089a4081d13e1bfe1d092f4d01dc402c50125
TAE JEONG KIM <monttj@gmail.com>
2017-03-03 06:22:43 +0900

    change python file name

--- topmass/job.sh
+++ topmass/job.sh
@@ -1,22 +1,22 @@
-python leptonweightWithUnfolded.py 167
-python leptonweightWithUnfolded.py 170
-python leptonweightWithUnfolded.py 173
-python leptonweightWithUnfolded.py 176
-python leptonweightWithUnfolded.py 179
+python leptonweight.py 167
+python leptonweight.py 170
+python leptonweight.py 173
+python leptonweight.py 176
+python leptonweight.py 179
 
-python leptonweightWithUnfolded.py 167_up
-python leptonweightWithUnfolded.py 170_up
-python leptonweightWithUnfolded.py 173_up
-python leptonweightWithUnfolded.py 176_up
-python leptonweightWithUnfolded.py 179_up
+python leptonweight.py 167_up
+python leptonweight.py 170_up
+python leptonweight.py 173_up
+python leptonweight.py 176_up
+python leptonweight.py 179_up
 
-python leptonweightWithUnfolded.py 167_dw
-python leptonweightWithUnfolded.py 170_dw
-python leptonweightWithUnfolded.py 173_dw
-python leptonweightWithUnfolded.py 176_dw
-python leptonweightWithUnfolded.py 179_dw
+python leptonweight.py 167_dw
+python leptonweight.py 170_dw
+python leptonweight.py 173_dw
+python leptonweight.py 176_dw
+python leptonweight.py 179_dw
 
-#python leptonweightWithUnfolded.py scaleup
-#python leptonweightWithUnfolded.py scalect
-#python leptonweightWithUnfolded.py scaledw
+#python leptonweight.py scaleup
+#python leptonweight.py scalect
+#python leptonweight.py scaledw
 
