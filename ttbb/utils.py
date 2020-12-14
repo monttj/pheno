afb1645ea125666f713f4755a872d15a950c5533
Physicsjong1 <physicsjong1@gmail.com>
2020-12-14 15:57:02 +0900

    update variables

--- ttbb/utils.py
+++ ttbb/utils.py
@@ -1,22 +1,24 @@
 import sys, os
 import math
-from tqdm import tqdm
 
 def getVarlist():
-    column = ['pt1', 'eta1', 'phi1', 'e1', 'pt2', 'eta2', 'phi2', 'e2',
-        'bbdR','bbdEta','bbdPhi','bbPt','bbEta',
-        'bbMass',
-        'nubbdR','nubbdEta','nubbdPhi','nubbPt','nubbMass','nubbHt',
-        'lbbdR','lbbdEta','lbbdPhi','lbbPt','lbbEta','lbbMass',
-        'nub1dR','nub1dEta','nub1dPhi','nub1Mass',
-        'nub2dR','nub2dEta','nub2dPhi','nub2Mass','nub2Ht',
-        'lb1dR','lb1dEta','lb1dPhi','lb1Pt','lb1Eta','lb1Mass','lb1Ht',
-        'lb2dR','lb2dEta','lb2dPhi','lb2Pt','lb2Eta','lb2Mass','lb2Ht'
+
+    column = ['bbdR','bbdEta','bbdPhi','bbPt','bbEta','bbMass','bbHt','bbMt',
+        'nubbdR','nubbdEta','nubbdPhi','nubbPt','nubbEta','nubbMass','nubbHt','nubbMt',
+        'lbbdR','lbbdEta','lbbdPhi','lbbPt','lbbEta','lbbMass','lbbHt','lbbMt',
+        'nub1dR','nub1dEta','nub1dPhi','nub1Pt','nub1Eta','nub1Mass','nub1Ht','nub1Mt',
+        'nub2dR','nub2dEta','nub2dPhi','nub2Pt','nub2Eta','nub2Mass','nub2Ht','nub2Mt',
+        'lb1dR','lb1dEta','lb1dPhi','lb1Pt','lb1Eta','lb1Mass','lb1Ht','lb1Mt',
+        'lb2dR','lb2dEta','lb2dPhi','lb2Pt','lb2Eta','lb2Mass','lb2Ht','lb2Mt',
 #        'csv1','csv2',
-#        'Wjb1dR','Wjb1dEta','Wjb1dPhi','Wjb1Pt','Wjb1Mass','Wjb1Ht',
-#        'Wjb2dR','Wjb2dEta','Wjb2dPhi','Wjb2Pt','Wjb2Mass','Wjb2Ht'
+        'Wjb1dR','Wjb1dEta','Wjb1dPhi','Wjb1Pt','Wjb1Eta','Wjb1Mass','Wjb1Ht','Wjb1Mt',
+        'Wjb2dR','Wjb2dEta','Wjb2dPhi','Wjb2Pt','Wjb2Eta','Wjb2Mass','Wjb2Ht','Wjb2Mt',
+        'pt1','pt2','eta1','eta2','e1','e2',
+        #'chi2','mtop'
     ]
-    return column
+    #No phi and chi2
+
+    print(len(column))
 #list backup
 #    column = ['bbdR','bbdEta','bbdPhi','bbPt','bbEta','bbPhi','bbMass','bbHt','bbMt',
 #        'nubbdR','nubbdEta','nubbdPhi','nubbPt','nubbEta','nubbPhi','nubbMass','nubbHt','nubbMt',
@@ -31,9 +33,22 @@ def getVarlist():
 #        'pt1','pt2','eta1','eta2','phi1','phi2','e1','e2',
 #    ]
 
-
+    #column = [
+    #    'dR','dEta', 'dPhi',
+    #    'nudR','nudPhi','nuPt','numT',
+    #    'lepdR','lepdEta', 'lepdPhi', 'lepPt', 'lepM',
+    #    'lb1dR', 'lb1dEta', 'lb1dPhi', 'lb1Pt', 'lb1M',
+    #    'lb2dR', 'lb2dEta', 'lb2dPhi', 'lb2Pt', 'lb2M',
+    #    'nub1dR', 'nub1dPhi', 'nub1Pt', 'nub1mT',
+    #    'nub2dR', 'nub2dPhi', 'nub2Pt', 'nub2mT',
+    #    'diPt','diEta','diM',
+    #    'b1Pt','b1E','b2Pt','b2E'
+    #]
+    return column
+getVarlist()
 
 def getHistXlabel():
+    #list backup
     dictionary = {
         'bbdR':'#DeltaR_{b#bar{b}}', 'bbdEta':'#Delta#eta_{b#bar{b}}','bbdPhi':'#Delta#phi_{b#bar{b}}','bbPt':'pT_{b#bar{b}}', 'bbEta':'#eta_{b#bar{b}}', 'bbPhi':'#phi_{b#bar{b}}', 'bbMass':'m_{b#bar{b}}','bbHt':'HT_{b#bar{b}}','bbMt':'mT_{b#bar{b}}',
         'nubbdR':'#DeltaR_{b#bar{b}#nu}', 'nubbdEta':'#Delta#eta_{b#bar{b}#nu}','nubbdPhi':'#Delta#phi_{b#bar{b}#nu}','nubbPt':'pT_{b#bar{b}#nu}', 'nubbEta':'#eta_{b#bar{b}#nu}', 'nubbPhi':'#phi_{b#bar{b}#nu}', 'nubbMass':'m_{b#bar{b}#nu}','nubbHt':'HT_{b#bar{b}#nu}','nubbMt':'mT_{b#bar{b}#nu}',
@@ -45,23 +60,28 @@ def getHistXlabel():
 #        'csv1':'csv_{b1}', 'csv2':'csv_{b2}',
         'Wjb1dR':'#DeltaR_{b1W_{j}}', 'Wjb1dEta':'#Delta#eta_{b1W_{j}}','Wjb1dPhi':'#Delta#phi_{b1W_{j}}','Wjb1Pt':'pT_{b1W_{j}}', 'Wjb1Eta':'#eta_{b1W_{j}}', 'Wjb1Phi':'#phi_{b1W_{j}}', 'Wjb1Mass':'m_{b1W_{j}}','Wjb1Ht':'HT_{b1W_{j}}','Wjb1Mt':'mT_{b1W_{j}}',
         'Wjb2dR':'#DeltaR_{b2W_{j}}', 'Wjb2dEta':'#Delta#eta_{b2W_{j}}','Wjb2dPhi':'#Delta#phi_{b2W_{j}}','Wjb2Pt':'pT_{b2W_{j}}', 'Wjb2Eta':'#eta_{b2W_{j}}', 'Wjb2Phi':'#phi_{b2W_{j}}', 'Wjb2Mass':'m_{b2W_{j}}','Wjb2Ht':'HT_{b2W_{j}}','Wjb2Mt':'mT_{b2W_{j}}',
-        'pt1':'pT_{b1}','pt2':'pT_{b2}', 'eta1':'#eta_{b1}', 'eta2':'#eta_{b2}','phi1':'#phi_{b1}', 'phi2':'#phi_{b2}', 'e1':'E_{b1}', 'e2':'E_{b2}'
+        'pt1':'pT_{b1}','pt2':'pT_{b2}', 'eta1':'#eta_{b1}', 'eta2':'#eta_{b2}','phi1':'#phi_{b1}', 'phi2':'#phi_{b2}', 'e1':'E_{b1}', 'e2':'E_{b2}',
+        'chi2':'#chi^{2}','mtop':'m_{top}',
     }
 
-    #list backup
-#    dictionary = {
-#        'bbdR':'#DeltaR_{b#bar{b}}', 'bbdEta':'#Delta#eta_{b#bar{b}}','bbdPhi':'#Delta#phi_{b#bar{b}}','bbPt':'pT_{b#bar{b}}', 'bbEta':'#eta_{b#bar{b}}', 'bbPhi':'#phi_{b#bar{b}}', 'bbMass':'m_{b#bar{b}}','bbHt':'HT_{b#bar{b}}','bbMt':'mT_{b#bar{b}}',
-#        'nubbdR':'#DeltaR_{b#bar{b}#nu}', 'nubbdEta':'#Delta#eta_{b#bar{b}#nu}','nubbdPhi':'#Delta#phi_{b#bar{b}#nu}','nubbPt':'pT_{b#bar{b}#nu}', 'nubbEta':'#eta_{b#bar{b}#nu}', 'nubbPhi':'#phi_{b#bar{b}#nu}', 'nubbMass':'m_{b#bar{b}#nu}','nubbHt':'HT_{b#bar{b}#nu}','nubbMt':'mT_{b#bar{b}#nu}',
-#        'lbbdR':'#DeltaR_{b#bar{b}l}', 'lbbdEta':'#Delta#eta_{b#bar{b}l}','lbbdPhi':'#Delta#phi_{b#bar{b}l}','lbbPt':'pT_{b#bar{b}l}', 'lbbEta':'#eta_{b#bar{b}l}', 'lbbPhi':'#phi_{b#bar{b}l}', 'lbbMass':'m_{b#bar{b}l}','lbbHt':'HT_{b#bar{b}l}','lbbMt':'mT_{b#bar{b}l}',
-#        'nub1dR':'#DeltaR_{b1#nu}', 'nub1dEta':'#Delta#eta_{b1#nu}','nub1dPhi':'#Delta#phi_{b1#nu}','nub1Pt':'pT_{b1#nu}', 'nub1Eta':'#eta_{b1#nu}', 'nub1Phi':'#phi_{b1#nu}', 'nub1Mass':'m_{b1#nu}','nub1Ht':'HT_{b1#nu}','nub1Mt':'mT_{b1#nu}',
-#        'nub2dR':'#DeltaR_{b2#nu}', 'nub2dEta':'#Delta#eta_{b2#nu}','nub2dPhi':'#Delta#phi_{b2#nu}','nub2Pt':'pT_{b2#nu}', 'nub2Eta':'#eta_{b2#nu}', 'nub2Phi':'#phi_{b2#nu}', 'nub2Mass':'m_{b2#nu}','nub2Ht':'HT_{b2#nu}','nub2Mt':'mT_{b2#nu}',
-#        'lb1dR':'#DeltaR_{b1l}', 'lb1dEta':'#Delta#eta_{b1l}','lb1dPhi':'#Delta#phi_{b1l}','lb1Pt':'pT_{b1l}', 'lb1Eta':'#eta_{b1l}', 'lb1Phi':'#phi_{b1l}', 'lb1Mass':'m_{b1l}','lb1Ht':'HT_{b1l}','lb1Mt':'mT_{b1l}',
-#        'lb2dR':'#DeltaR_{b2l}', 'lb2dEta':'#Delta#eta_{b2l}','lb2dPhi':'#Delta#phi_{b2l}','lb2Pt':'pT_{b2l}', 'lb2Eta':'#eta_{b2l}', 'lb2Phi':'#phi_{b2l}', 'lb2Mass':'m_{b2l}','lb2Ht':'HT_{b2l}','lb2Mt':'mT_{b2l}',
-##        'csv1':'csv_{b1}', 'csv2':'csv_{b2}',
-#        'Wjb1dR':'#DeltaR_{b1W_{j}}', 'Wjb1dEta':'#Delta#eta_{b1W_{j}}','Wjb1dPhi':'#Delta#phi_{b1W_{j}}','Wjb1Pt':'pT_{b1W_{j}}', 'Wjb1Eta':'#eta_{b1W_{j}}', 'Wjb1Phi':'#phi_{b1W_{j}}', 'Wjb1Mass':'m_{b1W_{j}}','Wjb1Ht':'HT_{b1W_{j}}','Wjb1Mt':'mT_{b1W_{j}}',
-#        'Wjb2dR':'#DeltaR_{b2W_{j}}', 'Wjb2dEta':'#Delta#eta_{b2W_{j}}','Wjb2dPhi':'#Delta#phi_{b2W_{j}}','Wjb2Pt':'pT_{b2W_{j}}', 'Wjb2Eta':'#eta_{b2W_{j}}', 'Wjb2Phi':'#phi_{b2W_{j}}', 'Wjb2Mass':'m_{b2W_{j}}','Wjb2Ht':'HT_{b2W_{j}}','Wjb2Mt':'mT_{b2W_{j}}',
-#        'pt1':'pT_{b1}','pt2':'pT_{b2}', 'eta1':'#eta_{b1}', 'eta2':'#eta_{b2}','phi1':'#phi_{b1}', 'phi2':'#phi_{b2}', 'e1':'E_{b1}', 'e2':'E_{b2}'
-#    }
+    #dictionary = {
+    #    'dR':'#DeltaR_{b#bar{b}}', 'dEta':'#Delta#eta_{b#bar{b}}','dPhi':'#Delta#phi_{b#bar{b}}',
+    #    'nudR':'#DeltaR_{b#bar{b}#nu}', 'nudPhi':'#Delta#phi_{b#bar{b}#nu}',
+    #    'nuPt':'pT_{b#bar{b}#nu}', 'numT':'mT_{b#bar{b}#nu}',
+    #    'lepdR':'#DeltaR_{b#bar{b}l}', 'lepdEta':'#Delta#eta_{b#bar{b}l}',
+    #    'lepdPhi':'#Delta#phi_{b#bar{b}l}', 'lepPt':'pT_{b#bar{b}l}', 'lepM':'m_{b#bar{b}l}',
+    #    'lb1dR':'#DeltaR_{b1l}', 'lb1dEta':'#Delta#eta_{b1l}', 'lb1dPhi':'#Delta#phi_{b1l}',
+    #    'lb1Pt':'pT_{b1l}', 'lb1M':'m_{b1l}',
+    #    'lb2dR':'#DeltaR_{b2l}', 'lb2dEta':'#Delta#eta_{b2l}', 'lb2dPhi':'#Delta#phi_{b2l}',
+    #    'lb2Pt':'pT_{b2l}', 'lb2M':'m_{b2l}',
+    #    'nub1dR':'#DeltaR_{b1#nu}', 'nub1dPhi':'#Delta#phi_{b1#nu}',
+    #    'nub1Pt':'pT_{b1#nu}', 'nub1mT':'mT_{b1#nu}',
+    #    'nub2dR':'#DeltaR_{b2#nu}', 'nub2dPhi':'#Delta#phi_{b2#nu}',
+    #    'nub2Pt':'pT_{b2#nu}', 'nub2mT':'mT_{b2#nu}',
+    #    'diPt':'pT_{b#bar{b}}', 'diEta':'#eta_{b#bar{b}}', 'diM':'m_{b#bar{b}}',
+    #    'b1Pt':'pT_{b1}', 'b1E':'E_{b1}', 'b2Pt':'pT_{b2}', 'b2E':'E_{b2}'
+    #}
+
     return dictionary
 
 def getHistRange(var):
@@ -86,6 +106,10 @@ def getHistRange(var):
         histRange = [20, 0, 400]
     elif 'csv' in var:
         histRange = [20, 0, 1]
+    elif 'chi2' in var:
+        histRange = [100, 0, 1000]
+    elif 'mtop' in var:
+        histRange = [70, 0, 350]
     else:
         histRange = [20, 0, 10]
         print("Variable does not exist")
